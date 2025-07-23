061b3e73812352a0e73aa8ebe6964508f93fc169
Venícios <venicioss@gmail.com>
2025-07-23 20:25:10 -0300

    Prátrica_02

---
+++ calculadora_soma_dados.py
@@ -0,0 +1,20 @@
+"""
+
+Leia 2 valores inteiros e armazene-os nas variáveis A e B. 
+
+Efetue a soma de A e B, atribuindo o seu resultado à variável X. 
+
+Entrada: A entrada contém 2 valores inteiros informados pelo usuário. 
+
+Saída: Imprima a mensagem "X = " (letra X maiúscula) seguido pelo valor da variável X e pelo final de linha.
+
+"""
+# Leitura dos valores
+A = int(input("Por favor, digite o primeiro valor: "))
+B = int(input("Por facor, digite o segundo valor: "))
+# Cálculo da soma
+X = A + B
+# Exibição do resultado
+#print(f"X = {X}")  
+
+print("X =",A + B)
